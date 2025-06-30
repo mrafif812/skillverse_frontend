ff1e2ba64d360b72cdbdd9b61304cd1d8fe9b83b
mrafif812 <mrafif812@gmail.com>
2025-07-01 04:34:46 +0500

    swiper_setup swiper js implemented

---
+++ src/components/dashboard/CoursesCard.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="max-w-72 h-96 shadow-lg rounded-xl bg-white border-[#f3f1f8] border-[1px] mb-5">
+        <a href="#" class="card">
+            <div class="relative h-1/2">
+                <img :src="image" class="w-full h-full object-cover rounded-t-xl" alt="">
+                <button @click.stop.prevent="toggleFavorite" class="absolute top-4 right-4 z-10 rounded-full bg-white px-2 py-1">
+                    <i :class="isFavorite ? 'fa-solid fa-heart text-red-500' : 'fa-regular fa-heart'"></i>
+                </button>
+                <span class="absolute left-4 top-5 text-xs bg-white rounded-xl px-2 py-1">{{ level }}</span>
+            </div>
+            <div class="p-4">
+                <h5 class="text-[#1E293B] text-base font-semibold mb-2 truncate">{{course_name}}</h5>
+                <div class="flex items-center justify-between mb-4">
+                    <div class="flex items-center justify-around text-sm font-thin text-[#6e798a]">
+                        <span>{{ rating }}</span>
+                        <div class="mx-2">
+                            <i class="fa-regular fa-star text-[#dedfe0]"></i>
+                        </div>
+                        <span>({{ number_of_reviews }} reviews)</span>
+                    </div>
+                    <button class="flex items-center justify-between rounded-md bg-[#754ffe] py-1 px-2">
+                        <div class="text-[10px] text-white mt-1">
+                            <i class="fa-solid fa-code-compare"></i>
+                        </div>
+                        <span class="text-xs text-white ml-1">compare</span>
+                    </button>
+                </div>
+                <div class="mb-5">
+                    <p class="line-clamp-2 text-[#6e798a] text-xs">{{ description }}</p>
+                </div>
+                <div class="flex justify-between items-center border-t border-[#676c7d3a] pt-3">
+                    <div class="flex items-center">
+                        <h5 class="text-base font-semibold text-[#1E293B] pr-2">${{price}}</h5>
+                        <p class="text-[#6e798a] text-xs"><s>${{discount}}</s></p>
+                    </div>
+                    <p class="text-xs">
+                        <i class="fa-regular fa-clock text-[#754ffe] mr-1"></i>
+                        <span class="text-[#6e798a]">{{ total_time }} Hours</span>
+                    </p>
+                </div>
+            </div>
+        </a>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps([
+    'image',
+    'is_favorite',
+    'level',
+    'course_name',
+    'rating',
+    'number_of_reviews',
+    'price',
+    'discount',
+    'total_time',
+    'description',
+]);
+const isFavorite = ref(false)
+
+function toggleFavorite() {
+  isFavorite.value = !isFavorite.value
+}
+
+</script>
--- src/components/dashboard/TopCourses.vue
+++ src/components/dashboard/TopCourses.vue
@@ -1,10 +1,133 @@
 <template>
-    <section class="bg-[#F8F7FF] sm:py-12 sm:mt-9">
-        <div class="custom-container">
-            <div>
-                <h1 class="sm:text-3xl sm:font-bold sm:mb-7 text-[#1E293B]">Top courses</h1>
-                <p class="sm:text-xs sm:mb-7 text-[#676C7D]">These are the most popular courses among listen courses learners worldwide</p>
-            </div>
+  <section class="bg-[#F8F7FF] sm:py-12 sm:mt-9">
+    <div class="custom-container-no-flex">
+      <div>
+        <h1 class="sm:text-3xl sm:font-bold sm:mb-7 text-[#1E293B]">Top courses</h1>
+        <p class="sm:text-xs sm:mb-7 text-[#676C7D]">These are the most popular courses among listen courses
+          learners worldwide</p>
+        <div class="mx-auto max-w-screen-xl px-4 relative box-border">
+          <swiper 
+            :modules="modules" 
+            :slides-per-view="4" 
+            :space-between="10" 
+            :navigation="{
+              nextEl: '.custom-next',
+              prevEl: '.custom-prev'
+            }" 
+            :loop="true" 
+            :scrollbar="{ draggable: true }" 
+            @swiper="onSwiper"
+            @slideChange="onSlideChange"
+          >
+            <swiper-slide v-for="course in courses" :key="course">
+              <courses-card :image="course.image" :is_favorite="course.is_favorite" :level="course.level"
+                :course_name="course.course_name" :rating="course.rating" :number_of_reviews="course.number_of_reviews"
+                :price="course.price" :discount="course.discount" :total_time="course.total_time"
+                :description="course.description">
+              </courses-card>
+            </swiper-slide>
+          </swiper>
+          <div
+            class="custom-prev absolute top-1/2 left-[-2rem] transform -translate-y-1/2 bg-white rounded-full w-10 h-10 flex items-center justify-center text-gray-700 shadow-md cursor-pointer z-10">
+            <span class="text-xl"><i class="fa-solid fa-chevron-left"></i></span>
+          </div>
+          <div
+            class="custom-next absolute top-1/2 right-[-2rem] transform -translate-y-1/2 bg-white rounded-full w-10 h-10 flex items-center justify-center text-gray-700 shadow-md cursor-pointer z-10">
+            <span class="text-xl"><i class="fa-solid fa-chevron-right"></i></span>
+          </div>
         </div>
-    </section>
+      </div>
+    </div>
+  </section>
 </template>
+
+<script setup>
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import { Navigation, Scrollbar, A11y } from 'swiper/modules';
+import { ref, onMounted } from 'vue';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import CoursesCard from './CoursesCard.vue';
+
+const modules = [Navigation, Scrollbar, A11y];
+
+const swiperRef = ref(null);
+const courses = ref([
+  {
+    image: 'https://images.unsplash.com/photo-1504384308090-c894fdcc538d?auto=format&fit=crop&w=600&q=80',
+    is_favorite: true,
+    level: 'Beginner',
+    course_name: 'Intro to Web Development',
+    rating: 4.5,
+    number_of_reviews: 1200,
+    price: 49.99,
+    discount: 110,
+    total_time: '04:00:00',
+    description: 'Node.js is a JavaScript runtime built on V8 engine that allows developers to run JavaScript code outside the browser. It uses an event-driven, non-blocking I/O model, which makes it lightweight and efficient. Node.js is commonly used to build scalable network applications, backend APIs, real-time services like chat apps, and more. It comes with a built-in package manager called npm (Node Package Manager), which provides access to thousands of open-source libraries. Developers often choose Node.js for its speed, scalability, and the ability to use the same language (JavaScript) on both the frontend and backend of web applications.'
+  },
+  {
+    image: 'https://images.unsplash.com/photo-1504384308090-c894fdcc538d?auto=format&fit=crop&w=600&q=80',
+    is_favorite: false,
+    level: 'Intermediate',
+    course_name: 'Vue.js Complete Guide',
+    rating: 4.7,
+    number_of_reviews: 850,
+    price: 59.99,
+    discount: 150,
+    total_time: '07:00:00',
+    description: 'Node.js is a JavaScript runtime built on V8 engine that allows developers to run JavaScript code outside the browser. It uses an event-driven, non-blocking I/O model, which makes it lightweight and efficient. Node.js is commonly used to build scalable network applications, backend APIs, real-time services like chat apps, and more. It comes with a built-in package manager called npm (Node Package Manager), which provides access to thousands of open-source libraries.'
+  },
+  {
+    image: 'https://images.unsplash.com/photo-1504384308090-c894fdcc538d?auto=format&fit=crop&w=600&q=80',
+    is_favorite: true,
+    level: 'Expert',
+    course_name: 'Advanced JavaScript Techniques',
+    rating: 4.9,
+    number_of_reviews: 2100,
+    price: 89.99,
+    discount: 100,
+    total_time: '14:00:00',
+    description: 'It uses an event-driven, non-blocking I/O model, which makes it lightweight and efficient. Node.js is commonly used to build scalable network applications, backend APIs, real-time services like chat apps, and more. It comes with a built-in package manager called npm (Node Package Manager), which provides access to thousands of open-source libraries. Developers often choose Node.js for its speed, scalability, and the ability to use the same language (JavaScript) on both the frontend and backend of web applications.'
+  },
+  {
+    image: 'https://images.unsplash.com/photo-1504384308090-c894fdcc538d?auto=format&fit=crop&w=600&q=80',
+    is_favorite: true,
+    level: 'Expert',
+    course_name: 'Advanced JavaScript Techniques',
+    rating: 4.9,
+    number_of_reviews: 2100,
+    price: 89.99,
+    discount: 100,
+    total_time: '14:00:00',
+    description: 'It uses an event-driven, non-blocking I/O model, which makes it lightweight and efficient. Node.js is commonly used to build scalable network applications, backend APIs, real-time services like chat apps, and more. It comes with a built-in package manager called npm (Node Package Manager), which provides access to thousands of open-source libraries. Developers often choose Node.js for its speed, scalability, and the ability to use the same language (JavaScript) on both the frontend and backend of web applications.'
+  },
+  {
+    image: 'https://images.unsplash.com/photo-1504384308090-c894fdcc538d?auto=format&fit=crop&w=600&q=80',
+    is_favorite: true,
+    level: 'Expert',
+    course_name: 'Advanced JavaScript Techniques',
+    rating: 4.9,
+    number_of_reviews: 2100,
+    price: 89.99,
+    discount: 100,
+    total_time: '14:00:00',
+    description: 'It uses an event-driven, non-blocking I/O model, which makes it lightweight and efficient. Node.js is commonly used to build scalable network applications, backend APIs, real-time services like chat apps, and more. It comes with a built-in package manager called npm (Node Package Manager), which provides access to thousands of open-source libraries. Developers often choose Node.js for its speed, scalability, and the ability to use the same language (JavaScript) on both the frontend and backend of web applications.'
+  },
+]);
+
+
+const onSwiper = (swiper) => {
+  swiperRef.value = swiper;
+};
+
+const onSlideChange = () => {
+  console.log('slide change');
+};
+
+onMounted(() => {
+  if (swiperRef.value) {
+    swiperRef.value.navigation.init();
+    swiperRef.value.navigation.update();
+  }
+});
+</script>
